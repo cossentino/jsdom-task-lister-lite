3f4a6410b8e35421e8eefa4866d61cac1cd21b06
Ian Cossentino <icossentino@gmail.com>
2021-02-22 17:02:54 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+const myForm = document.getElementById('create-task-form');
+
+
+myForm.onsubmit = function(e) {
+  e.preventDefault();
+  myTask = document.getElementById('new-task-description').value;
+  myList = document.getElementById('list');
+  let el = document.createElement('li');
+  let deleteButton = document.createElement('button');
+  el.innerHTML = myTask;
+  myList.appendChild(el)
+  return false;
+};
